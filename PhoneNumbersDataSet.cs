f70c5bd663328ccbab1aa2c19f123701dbe9bed5
alex <alexandru.ionica1234@gmail.com>
2020-11-07 14:59:52 +0200

    Add project files.

---
+++ PhoneNumbersDataSet.cs
@@ -0,0 +1,19 @@
+﻿namespace Pintea_Ionica_Lab5
+{
+}
+
+namespace Pintea_Ionica_Lab5
+{
+}
+
+namespace Pintea_Ionica_Lab5
+{
+}
+
+namespace Pintea_Ionica_Lab5
+{
+}
+
+namespace Pintea_Ionica_Lab5
+{
+}
